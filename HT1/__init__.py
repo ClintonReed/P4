fea21a0d415e51893b49ac2b15ad9e1742096c07
Clinton Reed <CREED0043@my.sullivan.edu>
2019-10-21 17:13:41 -0400

    verbage

--- HT1/__init__.py
+++ HT1/__init__.py
@@ -1,8 +1,8 @@
 import logging
-
+import math
 import azure.functions as func
 
-# Logging Info
+# Logging
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
