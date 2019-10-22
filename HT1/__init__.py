130a876f06c2db32dc246e06b7585fe1b92ee5a3
Clint Reed <creedprod@gmail.com>
2019-10-22 14:04:30 -0400

    attempted int gathering - getting run errors lib

--- HT1/__init__.py
+++ HT1/__init__.py
@@ -1,15 +1,18 @@
 import logging
-import math
 import azure.functions as func
 
 # Logging
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
 # Variables
 
-    num1 = req.params.get('num1')
+    num1 = req.params.get('int.num1')
+    num2 = req.params.get('int.num2')
+    num3 = 
+    total = (num1+num2)
 
 
 # Function
@@ -22,10 +25,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             num1 = req_body.get('num1')
 
-    if num1:
-        return func.HttpResponse(f"{num1}!")
+    if not num2:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            num2 = req_body.get('num2')
+
+    if num1 & num2:
+        return func.HttpResponse(f"{num1} - {num2} = {total}")
     else:
         return func.HttpResponse(
-             "Addition Function - Enter two numbers labeled num1 and num2 on the query string or in the request body",
-             status_code=400
+            "Addition Function - Enter two numbers labeled num1 and num2 on the query string or in the request body",
+            status_code=400
         )
