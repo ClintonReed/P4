1836929583e4023cde6451c7c1c54b0f53fac7e3
Clint Reed <creedprod@gmail.com>
2019-10-22 13:41:52 -0400

    Changed verbage - Added num1 variable

--- HT1/__init__.py
+++ HT1/__init__.py
@@ -9,23 +9,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
 # Variables
 
-    name = req.params.get('name')
+    num1 = req.params.get('num1')
 
 
 # Function
 
-    if not name:
+    if not num1:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            num1 = req_body.get('num1')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    if num1:
+        return func.HttpResponse(f"{num1}!")
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
+             "Addition Function - Enter two numbers labeled num1 and num2 on the query string or in the request body",
              status_code=400
         )
